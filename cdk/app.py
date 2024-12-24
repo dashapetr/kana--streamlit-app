021437c3c7a5f269a27beea9b9897c28222cc482
Darya Petrashka <dashacheb15@gmail.com>
2024-12-24 17:09:50 +0100

    add env configuration

--- cdk/app.py
+++ cdk/app.py
@@ -1,7 +1,11 @@
 from aws_cdk import App
+import aws_cdk as core
 from cdk.cdk_stack import CdkStack
 
+# Specify your AWS account ID and region
+env = core.Environment(account="YOUR_ACCOUNT_ID", region="us-east-1")
+
 app = App()
-CdkStack(app, "KanaStreamlitApp")
+CdkStack(app, "KanaStreamlitApp", env=env)
 
 app.synth()
